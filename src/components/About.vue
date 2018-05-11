3d6d833bef49832bb1f879b26a1ef9c743eb5b1f
kangyonggan <kangyonggan@gmail.com>
2018-05-11 11:42:33 +0800

    01

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+   这是首页
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Index'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
