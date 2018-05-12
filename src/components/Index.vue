a22282ad087063b123c5098b05cd8406f348bc8f
kangyonggan <kangyonggan@gmail.com>
2018-05-12 17:18:30 +0800

    List

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,238 +1,56 @@
 <template>
   <div>
-    <div class="left border">
-      <p>
-        <i class="fa fa-list"></i>
-        文章列表
-      </p>
-
-      <div class="split"></div>
-
-      <ul class="list">
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交</a>
-          <em>2018-05-12</em>
-        </li>
-        <li>
-          <div class="line"></div>
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交</a>
-          <em>2018-05-12</em>
-        </li>
-      </ul>
-
-      <ul class="pagination">
-        <li class="show"><a href="#">&lt;</a></li>
-        <li class="show"><a href="#">1</a></li>
-        <li class="active show"><a href="#">2</a></li>
-        <li class="show"><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#">6</a></li>
-        <li><a href="#">7</a></li>
-        <li><a href="#">8</a></li>
-        <li class="show"><a href="#">&gt;</a></li>
-      </ul>
+    <div class="left">
+      <List iconClass="fa fa-list" :em="true" :pagination="true" title="文章列表" :items="articles"/>
     </div>
 
-    <div class="right border">
-      <p>
-        <i class="fa fa-thumbs-up fa-fw"></i>
-        站长推荐
-      </p>
-
-      <div class="split"></div>
-
-      <ul class="list">
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="right border">
-      <p>
-        <i class="fa fa-edit"></i>
-        最近编辑
-      </p>
-
-      <div class="split"></div>
-
-      <ul class="list">
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-        <li class="sm">
-          <a href="#">SpringMVC结合拦截器、注解实现简单的防重复提交、注解实现简单的防重复提交</a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="right border">
-      <p>
-        <i class="fa fa-bookmark"></i>
-        收藏小说
-        <a class="more">
-          <i class="fa fa-plus"></i>
-        </a>
-      </p>
-
-      <div class="split"></div>
-
-      <ul class="list">
-        <li class="sm">
-          <div class="line"></div>
-          <a href="#">逆天邪神</a>
-          <em>火星引力</em>
-        </li>
-        <li class="sm">
-          <div class="line"></div>
-          <a href="#">武炼巅峰</a>
-          <em>火星引力</em>
-        </li>
-      </ul>
+    <div class="right">
+      <List iconClass="fa fa-thumbs-up fa-fw" :compact="true" title="站长推荐" :items="articles"/>
+      <List iconClass="fa fa-edit" :compact="true" title="最近编辑" :items="articles"/>
+      <List iconClass="fa fa-bookmark" :em="true" :compact="true" title="收藏小说" :items="novels"/>
     </div>
   </div>
 </template>
 
 <script>
+  import List from './List'
+
   export default {
-    name: 'Index'
+    components: {List},
+    name: 'Index',
+    data() {
+      return {
+        articles: [
+          {
+            title: "SpringMVC结合拦截器、注解实现简单的防重复提交",
+            href: "article/1",
+            em: "2018-05-12"
+          },
+          {
+            title: "SpringMVC结合拦截器、注解实现简单的防重复提交22222",
+            href: "article/1",
+            em: "2018-05-12"
+          }
+        ],
+        novels: [
+          {
+            title: "逆天邪神",
+            href: "article/1",
+            em: "火星引力"
+          },
+          {
+            title: "武炼巅峰",
+            href: "article/1",
+            em: "小莫"
+          }
+        ]
+      }
+    }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-  .left, .right {
-    margin-bottom: 10px;
-  }
-
-  p {
-    margin-left: 15px;
-    font-weight: bold;
-  }
-
-  .list {
-    list-style: none;
-    padding-left: 0;
-    margin: 0 15px;
-  }
-
-  .list li {
-    position: relative;
-    height: 40px;
-    line-height: 40px;
-    margin-top: 20px;
-    margin-bottom: 20px;
-  }
-
-  .list li.sm {
-    height: 30px;
-    line-height: 30px;
-    margin-top: 10px;
-    margin-bottom: 10px;
-  }
-
-  .list li .line {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 20px;
-    height: 1px;
-    border-bottom: 1px dashed #e8e8e8;
-    z-index: 1;
-  }
-
-  .list li.sm .line {
-    top: 15px;
-  }
-
-  .list li a {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    position: absolute;
-    left: 0;
-    top: 0;
-    max-width: 100%;
-    z-index: 9999;
-    background: #fff;
-    padding-right: 10px;
-  }
-
-  em {
-    position: absolute;
-    right: 0;
-    top: 0;
-    color: #ccc;
-    z-index: 9999;
-    background: #fff;
-    padding-left: 10px;
-    font-size: 13px;
-  }
-
   @media (min-width: 650px) {
     .left {
       float: left;
@@ -242,12 +60,6 @@
     .right {
       float: right;
       width: 27%;
-    }
-  }
-
-  @media (max-width: 1000px) {
-    em, .line {
-      display: none;
     }
   }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <div class="border">
+      <p>
+        <i :class="iconClass"></i>
+        {{title}}
+      </p>
+
+      <div class="split"></div>
+
+      <ul class="list">
+        <li v-for="item in items" :class="{compact: compact}">
+          <div class="line" v-show="em"></div>
+          <a :href="'/#/' + item.href">{{item.title}}</a>
+          <em v-show="em">{{item.em}}</em>
+        </li>
+      </ul>
+
+      <ul class="pagination" v-show="pagination">
+        <li class="show"><a href="#">&lt;</a></li>
+        <li class="show"><a href="#">1</a></li>
+        <li class="active show"><a href="#">2</a></li>
+        <li class="show"><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li><a href="#">6</a></li>
+        <li><a href="#">7</a></li>
+        <li><a href="#">8</a></li>
+        <li class="show"><a href="#">&gt;</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'List',
+    props: ["float", "iconClass", "pagination", "compact", "em", "title", "items"]
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  p {
+    margin-left: 15px;
+    font-weight: bold;
+  }
+
+  .list {
+    list-style: none;
+    padding-left: 0;
+    margin: 0 15px;
+  }
+
+  .list li {
+    position: relative;
+    height: 40px;
+    line-height: 40px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .list li.compact {
+    height: 30px;
+    line-height: 30px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  .list li .line {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 20px;
+    height: 1px;
+    border-bottom: 1px dashed #e8e8e8;
+    z-index: 1;
+  }
+
+  .list li.compact .line {
+    top: 15px;
+  }
+
+  .list li a {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    position: absolute;
+    left: 0;
+    top: 0;
+    max-width: 100%;
+    z-index: 9999;
+    background: #fff;
+    padding-right: 10px;
+  }
+
+  em {
+    position: absolute;
+    right: 0;
+    top: 0;
+    color: #ccc;
+    z-index: 9999;
+    background: #fff;
+    padding-left: 10px;
+    font-size: 13px;
+  }
+
+  @media (max-width: 1000px) {
+    em, .line {
+      display: none;
+    }
+  }
+</style>
+
+
